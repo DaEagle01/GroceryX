ebb6e53018bf71347e8f2f634f94affff76f80f8
Fahad Bin Karim <fbinkarim01@gmail.com>
2024-03-28 17:31:48 +0600

    updated loading state design

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Loading = () => {
     return (
-        <div className='w-full h-[90vh] max-w-[90rem] mx-auto flex justify-between sm:justify-start px-4 lg:px-20 py-12'>Loading...</div>
+        <div className='w-full h-[90vh] max-w-[90rem] mx-auto flex justify-center items-center gap-4 px-4 lg:px-20 py-12 '>
+            <div className="w-16 h-16 rounded-full bg-green-100 animate-pulse"></div>
+            <div className="w-24 h-24 rounded-full bg-green-100 animate-pulse"></div>
+            <div className="w-32 h-32 rounded-full bg-green-100 animate-pulse"></div>
+        </div>
     )
 }
 
